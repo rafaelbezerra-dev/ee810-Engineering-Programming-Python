ab75da0e6c1a8c7bc29a0e380202d7b1ff5bf696
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-02-29 23:37:39 -0500

    ..

---
+++ assignment_01/rt_process.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import re
+
+f = open("tr.txt", "r")
+lines = f.readlines()
+f.close()
+
+current_line = len(lines) - 1;
+
+while current_line >= 1:
+    if "* * *" in lines[current_line]:
+        current_line -= 1
+        continue
+
+    words = lines[current_line].split("  ");
+    if "ms" in words[len(words) - 1]:
+        ms = words[len(words) - 1].replace(" ms", "")
+        # float_ms = float(ms)
+        f = open("DelayValues.txt", "a")
+        f.write(ms)
+        f.close()
+        break
+    current_line -= 1
