fb45b841ce9c3de0f946a41d2a6d23edd20a1d27
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-03-02 23:03:31 -0500

    ..

--- assignment_01/trace.sh
+++ assignment_01/trace.sh
@@ -2,6 +2,12 @@
 #!/bin/bash
 
 cd /home/pi/Projects/GitHub/ee810/assignment_01/
-traceroute github.com >> tr.txt
-#chmod +x process.py
-python process.py
+
+traceroute -m 20 github.com >> tr_github.txt
+python process.py tr_github.txt dt_github.txt
+
+traceroute -m 20 jovemnerd.com.br >> tr_jovemnerd.txt
+python process.py tr_jovemnerd.txt dt_jovemnerd.tx
+
+traceroute -m 20 submarino.com.br >> tr_submarino.txt
+python process.py tr_submarino.txt dt_submarino.txt
