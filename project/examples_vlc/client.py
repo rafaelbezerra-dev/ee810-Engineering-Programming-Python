a8a2177dbdf76d7a16b5d24ef3ddd738762f18a9
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-03-31 17:26:55 -0400

    ..

---
+++ project/examples_vlc/client.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+# Echo client program
+import socket, sys
+
+def send(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    try:
+        sock.sendall(message)
+        response = sock.recv(1024)
+        if " " not in response:
+            response += " "
+        status_code, data = response.split(' ', 1)
+        return status_code
+        # print "Received: {}".format(response)
+    finally:
+        sock.close()
+
+HOST = 'localhost'    # The remote host
+PORT = 9997              # The same port as used by the server
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+# print HOST, PORT
+# send(HOST, PORT, "Hello World 1")
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+
+while True:
+    msg = raw_input('>> ')
+    send(HOST, PORT, msg)
+    if msg == "/quit" or msg == "/q":
+        break
+# s.sendall('Hello, world')
+# data = s.recv(1024)
+# print 'closing'
+# s.close()
+print 'bye'
+# print 'Received', repr(data)
--- project/server.py
+++ project/server.py
@@ -5,57 +5,7 @@ import vlc, pafy #https://github.com/mps-youtube/pafy
 import threading, socket, SocketServer
 
 from Queue import Queue
-
-class YouTubeVideo:
-    pass
-
-class YouTubePlayer:
-    def __init__(self):
-        self.instance = vlc.Instance('--no-fullscreen')
-        self.player = self.instance.media_player_new()
-        self.queue = Queue()
-        self.is_playing = False
-        self.is_media_set = False
-    def play(self):
-        if not self.is_playing:
-            if not self.is_media_set:
-                url = str(self.queue.get())
-                video = pafy.new(url)
-                url = video.getbest().url
-                self.player.set_media(self.instance.media_new(url))
-                self.queue.task_done()
-                self.is_media_set = True
-            self.player.play()
-        return
-    def pause(self):
-        if self.is_playing:
-            self.player.pause()
-        return
-    def stop(self):
-        self.player.stop()
-        return
-
-    def next(self):
-        url = str(self.queue.get())
-        video = pafy.new(url)
-        url = video.getbest().url
-        self.player.set_media(self.instance.media_new(url))
-        self.queue.task_done()
-        self.is_media_set = True
-        self.player.play()
-        return
-
-    def enqueue(self, url):
-        self.queue.put(url)
-        arr = [url for url in self.queue.queue]
-        print "QUEUE:"
-        index = 1
-        for url in arr:
-            print "  ", index, ": ", url
-            index += 1
-        return
-
-
+from youtube_lib import YouTubePlayer, YouTubeVideo
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
@@ -89,7 +39,6 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
         SocketServer.TCPServer.__init__(self, server_address, handler_class)
         return
 
-
 if __name__ == "__main__":
     # instance = vlc.Instance('--no-fullscreen')
     # url = "https://www.youtube.com/watch?v=OPf0YbXqDm0"
@@ -118,8 +67,10 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print "\nServer Interrupted"
 
-    print "Shutting down"
+    print "stopping player"
     player.stop()
+    print "shutting down"
     server.shutdown()
+    print "closing server"
     server.server_close()
     print "Bye"
