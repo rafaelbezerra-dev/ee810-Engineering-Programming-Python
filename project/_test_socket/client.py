0a8028e2778d78a6c0321a0b8df23c201a510251
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-04-02 13:20:19 -0400

    ..

--- project/_test_socket/client.py
+++ project/_test_socket/client.py
@@ -4,7 +4,7 @@
 import sys, socket
 
 HOST = sys.argv[1]#'localhost'    # The remote host
-PORT = sys.argv[2]#50007              # The same port as used by the server
+PORT = int(sys.argv[2])#50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 person = input('>> ')
